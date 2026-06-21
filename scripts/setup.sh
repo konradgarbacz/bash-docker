b1d1b4ae4de0f7d5a493bc0d5ebe5334ced8b95f
Konrad Garbacz <konrad.garbacz0@gmail.com>
2026-06-21 18:59:36 +0200

    change to script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,7 +5,6 @@ sudo apt update && sudo apt upgrade -y
 
 #install docker and add the current user to the docker group
 sudo curl -fsSL https://get.docker.com -o install-docker.sh
-sudo cat install-docker.sh
 sudo sh install-docker.sh
 sudo usermod -aG docker $USER
 
